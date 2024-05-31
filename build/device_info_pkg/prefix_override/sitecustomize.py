6d94dd9151733bca64fa288c9462b5fd4b35a0dc
summergerhart <summer.gerhart@dialexa.com>
2024-05-30 20:56:38 -0400

    install attempt 1

---
+++ build/device_info_pkg/prefix_override/sitecustomize.py
@@ -0,0 +1,3 @@
+import sys
+sys.real_prefix = sys.prefix
+sys.prefix = sys.exec_prefix = '/root/deepracer_ws/aws-deepracer-launcher/install/device_info_pkg'
---
+++ build/webserver_pkg/prefix_override/sitecustomize.py
@@ -0,0 +1,3 @@
+import sys
+sys.real_prefix = sys.prefix
+sys.prefix = sys.exec_prefix = '/root/deepracer_ws/aws-deepracer-launcher/install/webserver_pkg'
